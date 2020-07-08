7746c00ab40bea3e6e85676ddcba88a163f0909f
MemoVDG <memovdg@gmail.com>
2020-07-08 12:11:10 -0500

    Listed products

--- Back-End/routes/products.js
+++ Back-End/routes/products.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const ProductService = require('../services/products');
 
+/**
+ * Endpoints to get ML information
+ * @function productsAPI
+ * @param  express foo express object
+ * @return {[type]}     [description]
+ */
 function productsAPI(app) {
 	const router = express.Router();
 	app.use('/api/products', router);
 	const productService = new ProductService();
 
-	router.get('/:brand', async (req, res, next) => {
-		productService.getProducts().then((products) => {
+	router.get('/', async (req, res, next) => {
+		productService.getProducts()
+		.then((products) => {
 			res.status(200).send(products);
+		}).catch(err => {
+			next(err);
 		});
 	});
 }
--- Back-End/services/products.js
+++ Back-End/services/products.js
@@ -1,40 +1,67 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-const anAsyncFunction = async (sellerID) => {
-	const res = await axios.get(`https://api.mercadolibre.com/users/${sellerID}`);
-	return res.data;
-};
 
+/**
+ * Service to consult the ML information
+ * @class ProductService
+ */
 class ProductService {
+
+	/**
+	 * Return the array with the necesary information
+	 * @async
+	 * @function getProducts
+	 * @return {Promise<Array>} The seller's data.
+	 **/
 	async getProducts() {
 		let data = [];
 		const products = await this.requestProductsML();
 		const sellers = await this.requestSellers(products);
-
 		// Create the return object
 		products.map((product) => {
 			let seller = sellers.find((x) => x.id === product.seller.id);
+			
+			// Find the atributes
+			let attributes = {}
+			product.attributes.map(r => {
+				return attributes[r.name] = r.value_name
+			})
+
 			data.push({
-				Seller_ID: product.seller.id,
-				Seller_Name: seller.nickname,
+				Meli_ID : product.id,
+				Site_ID: product.site_id,
+				Titulo_Publicacion: product.title,
 				Marca: product.attributes.value_name,
 				Envio_Gratis: product.shipping.free_shipping,
 				Tipo_de_logistica: product.shipping.logistic_type,
 				Precio_de_Venta: product.price,
+				Link_Publicacion: product.permalink,
+				Cantidad_Disponible: product.available_quantity,
+				Condicion_Articulo: product.condition,
+				Seller_ID: product.seller.id,
+				Seller_Name: seller.nickname,
 				Lugar_de_Operaciones_del_Seller: `${seller.address.city} ${seller.address.state}`,
-				Condicion_articulo: product.condition,
+				Attributes: attributes
 			});
 		});
 
-		const dataOrderByPrice = _.sortBy(p, ['Precio_de_Venta']); //
-
+		// Sort by price
+		const dataOrderByPrice = _.sortBy(data, ['Precio_de_Venta']);
+		console.log(dataOrderByPrice.length);
 		return dataOrderByPrice || [];
 	}
-
+	
+	/**
+	 * Download the products from ML
+	 * @async
+	 * @function requestProductsML
+	 * @return {Promise<Array>} The seller's data.
+	 **/
 	async requestProductsML() {
 		let offset = 0;
 		let products = [];
+		// Get the first 1000
 		try {
 			while (offset < 1000) {
 				const response = await axios.get(
@@ -47,18 +74,25 @@ class ProductService {
 			}
 		} catch (err) {
 			console.log(err);
+			return [];
 		}
 		return products;
 	}
 
+	/**
+	 * Download the sellers from ML
+	 * @async
+	 * @function requestSellers
+	 * @param {Array} url - Products array with the seller's id.
+	 * @return {Promise<Array>} The seller's data.
+	 * */
 	async requestSellers(products) {
-		// Leave just unique seller id
+		// Keep just uniques seller id
 		const sellers = new Set();
 		products.map((product) => {
 			sellers.add(product.seller.id);
 		});
 
-		// Get the seller information
 		return Promise.all(
 			Array.from(sellers).map(async (seller) => {
 				const s = await axios.get(`https://api.mercadolibre.com/users/${seller}`);
